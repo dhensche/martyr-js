7ff12111a85e7fc85beee5bce91006463e20afe3
Derek Henscheid <derek-henscheid@uiowa.edu>
2013-11-02 18:39:08 -0500

    more ugliness

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var fs = require('fs'),
     path = require('path');
 
 function importSolutions(directory) {
+  exports[directory] = {};
   fs.readdir(path.join(__dirname, directory), function(err, files) {
     files.forEach(function(file) {
       if (path.extname(file) === '.js') {
         var solution = require('./' + path.join(directory, file));
-        exports[solution.name || file] = solution;
+        exports[directory][solution.name || file] = solution;
       }
     });
   });
